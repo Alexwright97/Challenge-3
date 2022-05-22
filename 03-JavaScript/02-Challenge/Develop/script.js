840d5a00c5facbba6459a091617aac508dbd779a
Alex_Wright <alexwright976@gmail.com>
2022-05-22 16:08:56 -0400

    looped the cahracters and numbers to randomaze

--- 03-JavaScript/02-Challenge/Develop/script.js
+++ 03-JavaScript/02-Challenge/Develop/script.js
@@ -16,10 +16,10 @@ const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
 const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
-  arrayFromLowToHigh(58, 64)
-  ).concat(arrayFromLowToHigh(91, 96)
-  ).concat(arrayFromLowToHigh(123, 126))
-)
+arrayFromLowToHigh(58, 64)
+).concat(arrayFromLowToHigh(91, 96)
+).concat(arrayFromLowToHigh(123, 126))
+
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
 
@@ -35,7 +35,21 @@ form.addEventListener('submit', e => {
 
 function generatePassword(characterAmount, includeUppercase,
 includeNumbers, includeSymbols)
-
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat
+  (UPPERCASE_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat
+  (SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat
+  (NUMBER_CHAR_CODES)
+  
+  
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount, i++){
+    const characterCode = charCodes[Math.floor(Math.random() *
+    characterAmount)]
+    passwordCharacters.push(String.fromCharCode(characterCodes))
+  }
 function arrayFromLowToHigh(low, high){
   const array = []
   for (let i = low; i <= high; i++){
