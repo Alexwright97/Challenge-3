1212e581c488def8b673aaadde3fa208ab4d58e7
Alex_Wright <alexwright976@gmail.com>
2022-05-22 13:52:40 -0400

    added number, character and symbols to password generator

--- 03-JavaScript/02-Challenge/Develop/script.js
+++ 03-JavaScript/02-Challenge/Develop/script.js
@@ -6,13 +6,8 @@ function writePassword()
 {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var length = (len)?(len):(10);
-  var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
-  var numeric = '0123456789';
-  var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-  var password = "";
-  var character = "";
-  var crunch = true;
+
+  
 }
 
 // Add event listener to generate button
