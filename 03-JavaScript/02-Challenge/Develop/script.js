a8013e4b39876d4ebea62d5b27bdccae95b9c55e
Alex_Wright <alexwright976@gmail.com>
2022-05-22 15:14:29 -0400

    created event argument

--- 03-JavaScript/02-Challenge/Develop/script.js
+++ 03-JavaScript/02-Challenge/Develop/script.js
@@ -1,13 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 const characterAmountRange = document.getElementById
-  ('characterAmountRange')
-  const characterAmountNumber = document.getElementById
-  ('characterAmountNumber')
+('characterAmountRange')
+const characterAmountNumber = document.getElementById
+('characterAmountNumber')
+const form = document.getElementById('passwordGeneratorForm')
 
-  characterAmountNumber.addEventListener('input', syncCharacterAmount)
-  characterAmountRange.addEventListener('input', syncCharacterAmount)
-// Write password to the #password input
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value =value
+}
+  // Write password to the #password input
 function writePassword() 
 {
   const password = generatePassword();
